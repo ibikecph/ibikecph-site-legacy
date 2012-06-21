e6145d8a1f9a2349ca205ce0cf15c84792bf4f90
Emil Tin <emil@tin.dk>
2012-06-21 16:06:20 +0200

    blog work

--- config/main_navigation.rb
+++ config/main_navigation.rb
@@ -4,7 +4,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :home, 'Map', root_path
     primary.item :blog, 'Feedback', feedback_path
-    primary.item :blog, 'Blog', blog_path
+    primary.item :blog, 'Blog', blog_entry_index_path, :highlights_on => /blog\//
     primary.item :about, 'Account', account_path
   end
 
