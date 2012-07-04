83250a507535065f4db08c60e64444023d8c8e9a
Emil Tin <emil@tin.dk>
2012-07-04 12:01:21 +0200

    rename lab to feedback

--- config/main_navigation.rb
+++ config/main_navigation.rb
@@ -2,6 +2,6 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :home, t('menu.routes'), root_path
     primary.item :blog, t('menu.blog'), blog_entry_index_path, :highlights_on => /^\/blog/
-    primary.item :blog, t('menu.lab'), issues_path
+    primary.item :blog, t('menu.feedback'), issues_path
   end
 end
