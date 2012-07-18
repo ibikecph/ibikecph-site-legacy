b8ac89a4e3ecede7bc722e3e76aac67158503b73
Emil Tin <emil@tin.dk>
2012-07-18 15:52:35 +0200

    fix missing menu highlighting when not in default locale

--- config/main_navigation.rb
+++ config/main_navigation.rb
@@ -1,7 +1,7 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :home, t('menu.routes'), root_path
-    primary.item :blog, t('menu.blog'), blog_entry_index_path, :highlights_on => /^\/blog/
-    primary.item :blog, t('menu.feedback'), issues_path
+    primary.item :blog, t('menu.blog'), blog_entry_index_path, :highlights_on => /^(\/en)?\/blog/
+    primary.item :blog, t('menu.feedback'), issues_path, :highlights_on => /^(\/en)?\/feedback/
   end
 end
