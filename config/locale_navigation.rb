ae1fd5a3dc13617c772cf77cd37b05e888635fa7
Emil Tin <emil@tin.dk>
2012-07-18 15:55:55 +0200

    don't show current language in menu

--- config/locale_navigation.rb
+++ config/locale_navigation.rb
@@ -1,10 +1,12 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
   	[:da,:en].each do |l|
-	    primary.item l, 
-	      t( l, :locale=>l ),
-	      url_for(:locale => (l == I18n.default_locale ? nil : l)), 
-        :highlights_on => lambda{ I18n.locale == l }
+  	  unless I18n.locale == l
+  	    primary.item l, 
+  	      t( l, :locale=>l ),
+  	      url_for(:locale => (l == I18n.default_locale ? nil : l)), 
+          :highlights_on => lambda{ I18n.locale == l }
+      end
   	end
   end
 end
