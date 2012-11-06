cfb961cec577100af316324ba15f0f1df9652ebf
Emil Tin <emil.tin@tmf.kk.dk>
2012-11-06 11:47:11 +0100

    make user role inaccessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :blog_entries, :dependent => :nullify
   has_many :themes, :dependent => :nullify
   has_many :comments, :dependent => :destroy
-  attr_accessible :role, :name, :about, :password, :password_confirmation, :image, :remove_image, :image_cache, :notify_by_email, :terms
+  attr_accessible :name, :about, :password, :password_confirmation, :image, :remove_image, :image_cache, :notify_by_email, :terms
   
   attr_accessor :password, :created_from_oath
   
