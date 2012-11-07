56428221dba2e57a6d48f6dc58f9114f13a41412
Emil Tin <emil.tin@tmf.kk.dk>
2012-11-07 15:42:17 +0100

    update join/leave corps, allow corps/ witout login

--- app/controllers/corps_controller.rb
+++ app/controllers/corps_controller.rb
@@ -1,4 +1,6 @@
 class CorpsController < ApplicationController
+
+  skip_before_filter :require_login, :only => [:index, :show]
   
   def index
     @users = User.where(:tester => true).order('name asc').paginate :page => params[:page], :per_page => 20
@@ -11,20 +13,20 @@ class CorpsController < ApplicationController
   end
   
   def join
-    if @current_user.tester
+    if current_user.tester
       flash[:notice] = t('corps.flash.already_joined')
     else
-      @current_user.update_attributes :tester => true
+      current_user.update_attributes :tester => true
       flash[:notice] = t('corps.flash.joined')
     end
     redirect_to corps_path
   end
   
   def leave
-    if !@current_user.tester
+    if !current_user.tester
       flash[:notice] = t('corps.flash.already_left')
     else
-      @current_user.update_attributes :tester => false
+      current_user.update_attributes :tester => false
       flash[:notice] = t('corps.flash.left')
     end
     redirect_to corps_path
