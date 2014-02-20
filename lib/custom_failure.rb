1d57b01f1dc86ceea3b1ea92ef66eb1ebaffaa20
Jacob V Thomas <jacob@pitsolutions.com>
2014-02-20 18:04:41 +0530

    confirm account updates

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -11,7 +11,7 @@ end
 def json_failure
 	self.status = 401 
 	self.content_type = 'json' 
-	self.response_body = {:success => false,:info => I18n.t('sessions.flash.confirm_account'),:errors => I18n.t("sessions.flash.confirm_account")}.to_json
+	self.response_body = {:success => false,:info_title => I18n.t('sessions.flash.confirm_account_title'),:info => I18n.t('sessions.flash.confirm_account'),:errors => I18n.t("sessions.flash.confirm_account")}.to_json
 end 
   
 end
