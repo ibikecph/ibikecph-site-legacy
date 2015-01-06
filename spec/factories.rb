9ff8b69aa64826453028396b5ce6f7b9f8e20c3d
Markus Faerevaag <m@frv.ag>
2015-01-06 16:45:31 +0100

    user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,14 @@ FactoryGirl.define do
     title 'Moby Dick'
     body 'Call me Ishmael.'
   end
+
+  factory :user do
+    name 'Moby Dick'
+    email 'moby@dick.com'
+
+    after :build do |u|
+      u.email_confirmation = u.email
+      u.password = u.password_confirmation = 'password'
+    end
+  end
 end
