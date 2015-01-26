65d508ed7acf845a50c223abd25ec7efd07d7133
Markus Faerevaag <m@frv.ag>
2015-01-26 16:19:42 +0100

    add factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,22 @@
+# coding: utf-8
 FactoryGirl.define do
+
+  sequence(:title) { |n| "Title#{n}" }
+  sequence(:name)  { |n| "Person#{n}" }
+  sequence(:email) { |n| "person#{n}@example.com" }
+
+  factory :user do
+    name
+    email
+
+    after :build do |u|
+      u.email_confirmation = u.email
+      u.password = u.password_confirmation = 'password'
+    end
+  end
+
   factory :issue do
-    title "Moby Dick"
+    title
     body "Call me Ishmael..."
   end
 
@@ -9,13 +25,24 @@ FactoryGirl.define do
     error_type "problemz"
   end
 
-  factory :user do
-    name 'Moby Dick'
-    email 'moby@dick.com'
+  factory :route do
+    from_name 'Vestergade 27-29, 1550 København V'
+    from_latitude '55.677276'
+    from_longitude '12.569467'
 
-    after :build do |u|
-      u.email_confirmation = u.email
-      u.password = u.password_confirmation = 'password'
-    end
+    to_name 'Lille Kannikestræde 3, 1170 København K'
+    to_latitude '55.679998'
+    to_longitude '12.574735'
+
+    start_date Date.new
+  end
+
+  factory :favourite do
+    name 'Fav'
+    address 'Vestergade 27-29, 1550 København V'
+    latitude '55.677276'
+    longitude '12.569467'
+    source 'favorite'
+    sub_source 'favorite'
   end
 end
