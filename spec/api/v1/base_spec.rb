98bc27a7b5fab3c2a756e13f80b1965652e04059
Markus Faerevaag <m@frv.ag>
2015-01-26 16:20:11 +0100

    add header spec

---
+++ spec/api/v1/base_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Base API', api: :v1 do
+
+  context 'should' do
+    it 'be reached with correct headers' do
+      get "/api/users", {}, headers
+
+      expect(response).not_to be_nil
+    end
+  end
+
+  context 'should not' do
+    it 'be reached without correct headers' do
+      expect{get "/api/users"}.to raise_error(ActionController::RoutingError)
+    end
+  end
+
+end
