ca51567844dcf52664c5a5944e6f96669702af0d
Sphinxo <sphinxominator@gmail.com>
2015-08-12 15:01:05 +0200

    facebook login possibly fixed for mobile

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,26 +6,28 @@ class Api::V1::SessionsController < Devise::SessionsController
 
   def create
     if params[:user][:fb_token]
-      fb = OmniAuth::Strategies::Facebook.new(
-        ENV['IBIKECPH_FBAPP_ID'],
-        ENV['IBIKECPH_FBAPP_SECRET']
-      )
-      client = ::OAuth2::Client.new(
-        ENV['IBIKECPH_FBAPP_ID'],
-        ENV['IBIKECPH_FBAPP_SECRET'],
-        fb.options.client_options
-      )
-      access_token = ::OAuth2::AccessToken.new(
-        client,
-        params[:user][:fb_token]
-      )
-      fb.instance_variable_set('@access_token', access_token)
+      #  fb = OmniAuth::Strategies::Facebook.new(
+      #    ENV['IBIKECPH_FBAPP_ID'],
+      #    ENV['IBIKECPH_FBAPP_SECRET']
+      # )
+      # client = ::OAuth2::Client.new(
+      #   ENV['IBIKECPH_FBAPP_ID'],
+      #   ENV['IBIKECPH_FBAPP_SECRET'],
+      #   fb.options.client_options
+      # )
+      # access_token = ::OAuth2::AccessToken.new(
+      #   client,
+      #   params[:user][:fb_token]
+      # )
+      # fb.instance_variable_set('@access_token', access_token)
+      #
+      # @fbauth_hash = fb.auth_hash rescue nil
+      # return failure unless @fbauth_hash
 
-      @fbauth_hash = fb.auth_hash rescue nil
-      return failure unless @fbauth_hash
+      fb_user = Koala::Facebook::API.new(params[:user][:fb_token]).get_object('me', fields: 'id,name,email')
 
-      if @fbauth_hash
-        @user = User.find_for_facebook_oauth(@fbauth_hash)
+      if fb_user
+        @user = User.find_for_facebook_uid(fb_user)
         return failure unless @user
 
         if @user
