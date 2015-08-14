c405d8a65c4e38636cb1d4ea676cb9a2aaea6a5b
Viktor Shamal Andersen <sphinxominator@gmail.com>
2015-08-14 14:41:40 +0200

    Update sessions_controller.rb

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::SessionsController < Devise::SessionsController
 
   skip_before_filter :check_auth_token!
+  skip_before_filter :verify_authenticity_token
   prepend_before_filter :check_login_params, only: [:create]
   prepend_before_filter :require_no_authentication, only: [:create]
 
