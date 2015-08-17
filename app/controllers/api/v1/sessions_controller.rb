0768d82b36f7c8145618abccb2f8ce6bb6d66584
Viktor Shamal Andersen <sphinxominator@gmail.com>
2015-08-17 14:02:59 +0200

    Update sessions_controller.rb

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -27,7 +27,7 @@ class Api::V1::SessionsController < Devise::SessionsController
       fb_user = Koala::Facebook::API.new(params[:user][:fb_token]).get_object('me', fields: 'id,name,email')
 
       if fb_user
-        @user = User.find_for_facebook_uid(fb_user)
+        @user = User.find_for_facebook_email(fb_user)
         return failure unless @user
 
         if @user
