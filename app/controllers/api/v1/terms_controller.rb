1b46f88a73cef6f50560a82a19853a959daba65d
Sphinxo <sphinxominator@gmail.com>
2015-09-01 09:58:54 +0200

    fixed terms

--- app/controllers/api/v1/terms_controller.rb
+++ app/controllers/api/v1/terms_controller.rb
@@ -6,11 +6,9 @@ class Api::V1::TermsController < Api::V1::BaseController
     render status: 200,
            json: {
              # This is not elegant.
-             version: t('terms.version', locale: :da).to_i,
-             important_parts_description_da: t('terms.important_parts_description', locale: :da),
-             important_parts_description_en: t('terms.important_parts_description', locale: :en)
+             version: ENV['terms_version'],
+             important_parts_description_da: t("mobileterms.v#{ENV['terms_version']}.important_parts_description", locale: :da),
+             important_parts_description_en: t("mobileterms.v#{ENV['terms_version']}.important_parts_description", locale: :en)
            }
-    
   end
-
 end
