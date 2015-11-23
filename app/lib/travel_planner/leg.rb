fd9f25fc0f02e6585e15788cb78d04e89ea34b75
Sphinxo <sphinxominator@gmail.com>
2015-11-23 15:33:06 +0100

    arrival and departure time is now returned as unix timestamp

--- app/lib/travel_planner/leg.rb
+++ app/lib/travel_planner/leg.rb
@@ -15,27 +15,25 @@ class TravelPlanner::Leg
     @name ||= @data['name']
   end
 
-  def departure_time
-    @departure_time ||= @data['Origin']['time']
-  end
-
-  def arrival_time
-    @arrival_time ||= @data['Destination']['time']
-  end
-
   def type
     @type ||= @data['type']
   end
 
+  def departure_time
+    @departure_time ||= parse_time(origin)
+  end
+
+  def arrival_time
+    @arrival_time ||= parse_time(destination)
+  end
+
   def total_time
-    @total_time ||= parse_time
+    @total_time ||= (arrival_time-departure_time)
   end
 
   private
-  def parse_time
+  def parse_time(point)
     format = '%d.%m.%y%H:%M'
-    start_time =  DateTime.strptime(origin['date'] + origin['time'],format)
-    end_time   =  DateTime.strptime(destination['date'] + destination['time'],format)
-    ((end_time - start_time) * 1.days).floor
+    DateTime.strptime(point['date'] + point['time'],format).to_i
   end
 end
