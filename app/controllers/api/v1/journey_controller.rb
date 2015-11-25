fdff1d821b6984a78b0fa95a60eeece9090edd26
Sphinxo <sphinxominator@gmail.com>
2015-11-25 16:02:17 +0100

    controller now gracefully fails while still logging exceptions

--- app/controllers/api/v1/journey_controller.rb
+++ app/controllers/api/v1/journey_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::JourneyController < Api::V1::BaseController
   skip_before_filter :check_auth_token!
 
   rescue_from TravelPlanner::Error, with: :error_message
+  rescue_from StandardError, with: :standard_message
 
   def show
     render json: TravelPlanner.get_journey(params[:loc])
@@ -9,5 +10,10 @@ class Api::V1::JourneyController < Api::V1::BaseController
 
   def error_message(e)
     render json: {error: e.message}, status: 422
+  end
+
+  def standard_message(e)
+    ExceptionNotifier.notify_exception(e)
+    render json: {error: 'An unexpected error occurred.'}, status: 500
   end
 end
