3e47b9106669d2b5a0b2d93e20133fc21786bac0
Emil Tin <emil.tin@tmf.kk.dk>
2016-04-25 18:51:25 +0200

    use modified-date in terms api to enable caching, also return full text

--- app/controllers/api/v1/terms_controller.rb
+++ app/controllers/api/v1/terms_controller.rb
@@ -3,13 +3,16 @@ class Api::V1::TermsController < Api::V1::BaseController
   skip_before_filter :check_auth_token!
   
   def index
-    render status: 200,
-           json: {
-             version: ENV['TERMS_VERSION'],
-             important_parts_description_da: t("mobileterms.v#{ENV['TERMS_VERSION']}.important_parts_description", locale: :da),
-             important_parts_description_en: t("mobileterms.v#{ENV['TERMS_VERSION']}.important_parts_description", locale: :en)
-           }
+    if stale?(last_modified: TERMS_LAST_MODIFIED)
+      render status: 200,
+             json: {
+               important_parts_description_da: t("user_terms.summary", locale: :da),
+               important_parts_description_en: t("user_terms.summary", locale: :en),
+               full_text_da: t("user_terms.full_text", locale: :da),
+               full_text_en: t("user_terms.full_text", locale: :en)
+             }
+    end
     
   end
-
+  
 end
--- spec/api/v1/terms_spec.rb
+++ spec/api/v1/terms_spec.rb
@@ -3,13 +3,34 @@ require 'rails_helper'
 describe 'Terms API', api: :v1 do
 
   context 'should' do
-    it 'get terms' do
+    it 'get terms if HTTP_IF_MODIFIED_SINCE is not set' do
       get "/api/terms", {}, headers
-      
       expect(response).to be_success
       expect(response).to have_http_status(200)
-
-      expect(json['version']).to eq ENV['TERMS_VERSION']
+      expect(response.headers['Last-Modified']).to eq( TERMS_LAST_MODIFIED.httpdate )
+      expect(json['important_parts_description_da']).to eq( I18n.t("user_terms.summary", locale: :da) )
+      expect(json['important_parts_description_en']).to eq( I18n.t("user_terms.summary", locale: :en) )
+      expect(json['full_text_da']).to eq( I18n.t("user_terms.full_text", locale: :da) )
+      expect(json['full_text_en']).to eq( I18n.t("user_terms.full_text", locale: :en) )
     end
+  end
+
+  it 'return 304 Not Modified if HTTP_IF_MODIFIED_SINCE is later than modifed date' do
+    fetched = TERMS_LAST_MODIFIED + 1.minute
+    get "/api/terms", {}, headers.merge( "HTTP_IF_MODIFIED_SINCE": fetched.httpdate )
+    expect(response).to have_http_status(304)
+    expect(response.headers['Last-Modified']).to eq( TERMS_LAST_MODIFIED.httpdate )
+  end
+
+  it 'return terms if HTTP_IF_MODIFIED_SINCE is earlier than modifed date' do
+    fetched = TERMS_LAST_MODIFIED - 1.minute
+    get "/api/terms", {}, headers.merge( "HTTP_IF_MODIFIED_SINCE": fetched.httpdate )
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
+    expect(response.headers['Last-Modified']).to eq( TERMS_LAST_MODIFIED.httpdate )
+    expect(json['important_parts_description_da']).to eq( I18n.t("user_terms.summary", locale: :da) )
+    expect(json['important_parts_description_en']).to eq( I18n.t("user_terms.summary", locale: :en) )
+    expect(json['full_text_da']).to eq( I18n.t("user_terms.full_text", locale: :da) )
+    expect(json['full_text_en']).to eq( I18n.t("user_terms.full_text", locale: :en) )
   end
 end
